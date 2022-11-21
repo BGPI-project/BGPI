f4b526de7ea9839c1297920e26fe6c081f7b0c6a
Jesús Martín Vergara <jmartin@youforget.me>
2022-11-21 18:39:35 +0100

    modelos creados

--- core/settings.py
+++ core/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'apps.home'  # Enable the inner home (home)
+    'apps.home',
+    'gestionPedidos'
+
 ]
 
 MIDDLEWARE = [
--- core/urls.py
+++ core/urls.py
@@ -10,6 +10,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),          # Django admin route
     path("", include("apps.authentication.urls")), # Auth routes - login / register
 
+    path("cart/", include("gestionPedidos.urls")), # Auth routes - login / register
     # ADD NEW Routes HERE
 
     # Leave `Home.Urls` as last the last line
